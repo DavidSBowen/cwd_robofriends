0850453c0964012603ffc2dc3224c42d45798f1f
David Bowen <dsbmusic328@gmail.com>
2018-02-28 10:45:02 -0500

    changed draggability of pictures

--- robofriends/src/Components/Card.js
+++ robofriends/src/Components/Card.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../Styles/App.css';
 
-const Card = ({name, email, id}) => {
-    return (
-        <div draggable="true" className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${id}?size=200x200`} alt="" />
+class Card extends Component {
+    render() {
+        const { name, email, id } = this.props;
+        return (
+            <div draggable="true" className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img draggable="false" src={`https://robohash.org/${id}?size=200x200`} alt="" />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
             </div>
         </div>
-    );
+        )
+    }
 }
+
+// const Card = ({ name, email, id }) => {
+//     return (
+//         <div draggable="true" className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+//             <img draggable="false" src={`https://robohash.org/${id}?size=200x200`} alt="" />
+//             <div>
+//                 <h2>{name}</h2>
+//                 <p>{email}</p>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default Card;
